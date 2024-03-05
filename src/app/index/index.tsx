07a52e30f710e19139068eee2a3bf4a99d891094
Marcos Renê <renemoura789@gmail.com>
2024-03-05 12:50:18 -0300

    feat: add expo fonts

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -5,7 +5,14 @@ import { styles } from './styles'
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello, Marcos!</Text>
+      <Text style={styles.title}>
+        Escolha {'\n'}
+        <Text style={styles.subtitle}>os produtos</Text>
+      </Text>
+
+      <Text style={styles.message}>
+        Descubra receitas baseadas nos produtos que você escolheu.
+      </Text>
     </View>
   )
 }
--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -5,8 +5,22 @@ import { theme } from '@/theme'
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    padding: 24,
   },
-  title: {},
-  subtitle: {},
+  title: {
+    marginTop: 42,
+    fontFamily: theme.fonts.family.bold,
+    fontSize: theme.fonts.size.heading.xl,
+    lineHeight: 44,
+  },
+  subtitle: {
+    fontFamily: theme.fonts.family.regular,
+  },
+  message: {
+    marginTop: 12,
+    marginBottom: 38,
+    fontFamily: theme.fonts.family.medium,
+    fontSize: theme.fonts.size.body.md,
+    color: theme.colors.gray_400,
+  },
 })
