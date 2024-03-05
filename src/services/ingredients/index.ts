2be3daf32517883cf3128c6108b2d4b728ce5aa8
Marcos Renê <renemoura789@gmail.com>
2024-03-05 16:27:49 -0300

    feat: add preparations and recipes services

--- src/services/ingredients/index.ts
+++ src/services/ingredients/index.ts
@@ -1,7 +1,28 @@
-import { subpabse } from '../supabase'
+import { supabase } from '../supabase'
+
+export async function findByIds(ids: string[]) {
+  const { data } = await supabase
+    .from('ingredients')
+    .select()
+    .in('id', ids)
+    .order('name')
+    .returns<IngredientResponse[]>()
+
+  return data ?? []
+}
+
+export async function findByRecipeId(id: string) {
+  const { data } = await supabase
+    .from('recipes_ingredients')
+    .select('ingredients (id, name, image)')
+    .eq('recipe_id', id)
+    .returns<{ ingredients: IngredientResponse }[]>()
+
+  return data ? data.map((item) => item.ingredients) : []
+}
 
 export async function findAll() {
-  const { data } = await subpabse
+  const { data } = await supabase
     .from('ingredients')
     .select()
     .order('name')
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -4,4 +4,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseURL = process.env.EXPO_PUBLIC_SUPABASE_URL ?? ''
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? ''
 
-export const subpabse = createClient(supabaseURL, supabaseKey)
+export const supabase = createClient(supabaseURL, supabaseKey)
